f0787c0838db307347ce4ad221c03623ddaa7071
Ali <babagaddafi1@gmail.com>
2026-07-24 17:05:36 -0400

    Increase EKS node capacity

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -154,9 +154,9 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = var.node_instance_types
 
-      min_size     = 1
+      min_size     = 2
       max_size     = 2
-      desired_size = 1
+      desired_size = 2
     }
   }
 
